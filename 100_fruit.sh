42c3feed0e5c071c4cb8bd7f285ef90a9fafe531
Joel Epstein <joel.epstein@greymatter.io>
2022-10-09 09:05:12 +0200

    updated 100 scripts and EXPORTS

--- 100_fruit.sh
+++ 100_fruit.sh
@@ -6,7 +6,7 @@
 # they need to be grouped for the eval within EXPORTS.cue
 # kiwi peach orange fig grape
 
-for i in {1..50}
+for i in {1..100}
 do
 #echo "kiwi${i}"
 ~/greymatter init service --insecure --type=http --dir greymatter/grocerylist --port=3000 --package=grocerylist "kiwi-${i}"
