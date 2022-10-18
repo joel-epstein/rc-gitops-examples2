d4c0278c7a57efcae74e2c9a7e6fcfbb1feee134
Joel Epstein <joel.epstein@greymatter.io>
2022-10-18 18:38:43 +0200

    add melon

--- 100_fruit.sh
+++ 100_fruit.sh
@@ -6,13 +6,18 @@
 # they need to be grouped for the eval within EXPORTS.cue
 # kiwi peach orange fig grape
 
-for i in {1..100}
+for i in {1..50}
 do
 #echo "kiwi${i}"
 #~/greymatter init service --insecure --type=http --dir greymatter/grocerylist --port=3000 --package=grocerylist "kiwi${i}"
 #~/greymatter init service --insecure --type=http --dir greymatter/grocerylist --port=3000 --package=grocerylist "peach${i}"
 #~/greymatter init service --insecure --type=http --dir greymatter/grocerylist --port=3000 --package=grocerylist "orange${i}"
 #~/greymatter init service --insecure --type=http --dir greymatter/grocerylist --port=3000 --package=grocerylist "fig${i}"
-~/greymatter init service --insecure --type=http --dir greymatter/grocerylist --port=3000 --package=grocerylist "grape${i}"
+#~/greymatter init service --insecure --type=http --dir greymatter/grocerylist --port=3000 --package=grocerylist "grape${i}"
+~/greymatter init service --insecure --type=http --dir greymatter/grocerylist --port=3000 --package=grocerylist "melon${i}"
+#~/greymatter init service --insecure --type=http --dir greymatter/grocerylist --port=3000 --package=grocerylist "cranberry${i}"
+#~/greymatter init service --insecure --type=http --dir greymatter/grocerylist --port=3000 --package=grocerylist "star${i}"
+#~/greymatter init service --insecure --type=http --dir greymatter/grocerylist --port=3000 --package=grocerylist "tangerine${i}"
+#~/greymatter init service --insecure --type=http --dir greymatter/grocerylist --port=3000 --package=grocerylist "blueberry${i}"
 done
 
